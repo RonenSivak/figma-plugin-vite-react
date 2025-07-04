2b549fe2da84894f5d05cb09e7fa2826df07b89a
Ronen Sivak <140695222+RonenSivak@users.noreply.github.com>
2025-07-04 19:12:25 +0300

    Initial commit: Figma Plugin React Vite template with Yarn migration

---
+++ src/plugin/index.ts
@@ -0,0 +1,31 @@
+/// <reference types="@figma/plugin-typings" />
+
+import { PLUGIN, UI } from "../common/networks";
+import { PLUGIN_CHANNEL } from "./index.network";
+import { Networker } from "monorepo-networker";
+
+async function bootstrap() {
+  Networker.initialize(PLUGIN, PLUGIN_CHANNEL);
+
+  if (figma.editorType === "figma") {
+    figma.showUI(__html__, {
+      width: 800,
+      height: 650,
+      title: "My Figma Plugin!",
+    });
+  } else if (figma.editorType === "figjam") {
+    figma.showUI(__html__, {
+      width: 800,
+      height: 650,
+      title: "My FigJam Plugin!",
+    });
+  }
+
+  console.log("Bootstrapped @", Networker.getCurrentSide().name);
+
+  PLUGIN_CHANNEL.emit(UI, "hello", ["Hey there, UI!"]);
+
+  setInterval(() => PLUGIN_CHANNEL.emit(UI, "ping", []), 5000);
+}
+
+bootstrap();
